05a254a50e9d6591300c26a2ffdb4b62517781b3
wallace <wallaceleite915@gmail.com>
2024-04-04 23:39:49 -0300

    inserindo imagens de retorno a resposta + visual da aplicacao

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 var meuBotao = document.querySelectorAll('.opcao');
+var meuMenu = document.querySelectorAll('.botao');
 var meuAudio = document.getElementById('audioOpcoes');
+var myAudio = document.getElementById('myAudio');
+
+
 
 meuBotao.forEach(function(botao){
     botao.addEventListener('mouseenter', function(){
-        meuAudio.currentTime = 0;
-        meuAudio.play();
+       meuAudio.currentTime = 0;
+       meuAudio.play();
     })
 })
+
+meuMenu.forEach(function(myMenu){
+    myMenu.addEventListener('mouseenter', function(){
+       myAudio.currentTime = 0;
+       myAudio.play();
+    })
+})
+
 
 function tocar(){
     var audio = document.getElementById("audio");
@@ -55,113 +67,138 @@ const questoes = [
         ],
         resposta: "Hinata , Shino e Kiba"
     },
-    /*
+    
     {
         pergunta: "Quais sao os 3 classes de nivel basico de um ninja?",
-        opcao1: ["Gennin, Chunnin e Jonnin"],
-        opcao2: ["Ambu, jonnin e Sannin"],
-        opcao3: ["Chunnin, kage e mizukage"],
-        opcao4: ["Gennin, Jonnin e kazekage"],
+        opcoes:[
+            ["Gennin, Chunnin e Jonnin"],
+            ["Ambu, jonnin e Sannin"],
+            ["Chunnin, kage e mizukage"],
+            ["Gennin, Jonnin e kazekage"],
+    ],
         resposta: "Gennin, Chunnin e Jonnin"
     },
     
     {
         pergunta: "Quais os viloes principais na saga da 'ponte da nevoa'?",
-        opcao1: ["Gaara e temari"],
-        opcao2: ["Haku e Zabuza"],
-        opcao3: ["Mizuki e Rokusho"],
-        opcao4: ["Irmaos Demonios"],
+        opcoes:[
+            ["Gaara e temari"],
+            ["Haku e Zabuza"],
+            ["Mizuki e Rokusho"],
+            ["Irmaos Demonios"],
+    ],
         resposta: "Haku e Zabuza"
     },
     {
         pergunta: "No primeiro episodio de naruto classico, quem e o vilão que aparece?",
-        opcao1: ["Rasa"],
-        opcao2: ["Itachi"],
-        opcao3: ["Mizuki"],
-        opcao4: ["Kimimaru"],
+        opcoes:[
+            ["Rasa"],
+            ["Itachi"],
+            ["Mizuki"],
+            ["Kimimaru"],
+        ],
         resposta: "Mizuki"
     },
     {
         pergunta: "Qual o personagem que atacou sasuke durante o exame chunnin na 'Floresta da morte'?",
-        opcao1: ["Gaara"],
-        opcao2: ["Orochimaru"],
-        opcao3: ["Kabuto"],
-        opcao4: ["Itachi"],
+        opcoes:[
+            ["Gaara"],
+            ["Orochimaru"],
+            ["Kabuto"],
+            ["Itachi"],
+        ],
         resposta: "Orochimaru"
     },
     {   
         pergunta: "Quem conseguiu pegas os guinzos durante o treinamento do kakashi ao time 7?",
-        opcao1: ["Sakura e Naruto"],
-        opcao2: ["Naruto e Sasuke"],
-        opcao3: ["Sakura e Sasuke"],
-        opcao4: ["Naruto"],
+        opcoes:[
+            ["Sakura e Naruto"],
+            ["Naruto e Sasuke"],
+            ["Sakura e Sasuke"],
+            ["Naruto"],
+        ],
         resposta: "Sakura e Sasuke"
     },
     {
         pergunta: "Qual foi o primeiro jutsu que o naruto conseguiu aprender?",
-        opcao1: ["Rasengan"],
-        opcao2: ["Futon Rasen Shuriken"],
-        opcao3: ["Jutsu Sexy"],
-        opcao4: ["Shadow clone jutsu"],
+        opcoes:[
+            ["Rasengan"],
+            ["Futon Rasen Shuriken"],
+            ["Jutsu Sexy"],
+            ["Shadow clone jutsu"],
+        ],
         resposta: "Shadow clone jutsu"
     },
     {
         pergunta: "Qual o desejo principal do naruto?",
-        opcao1: ["Virar programador"],
-        opcao2: ["Ser o hokage"],
-        opcao3: ["Seguir o sasuke"],
-        opcao4: ["Namorar a sakura"],
-        resposta: "Ser o hokage da vila" 
+        opcoes:[
+            ["Virar programador"],
+            ["Ser o hokage"],
+            ["Seguir o sasuke"],
+            ["Namorar a sakura"],
+        ],
+        resposta: "Ser o hokage" 
     },
     {
         pergunta: "Com quem foi o primeiro beijo do naruto?",
-        opcao1: ["Sasuke"],
-        opcao2: ["Sakura"],
-        opcao3: ["Hinata"],
-        opcao4: ["Tenten"],
+        opcoes:[
+            ["Sasuke"],
+            ["Sakura"],
+            ["Hinata"],
+            ["Tenten"],
+        ],
         resposta: "Sasuke"
     },
     {
         pergunta: "No final do exame chunnin, quem consegue evoluir de nivel para chunnin?",
-        opcao1: ["Sasuke"],
-        opcao2: ["Naruto"],
-        opcao3: ["Neji"],
-        opcao4: ["Shikamaru"],
+        opcoes:[
+            ["Sasuke"],
+            ["Naruto"],
+            ["Neji"],
+            ["Shikamaru"],
+        ],
         resposta: "Shikamaru"
     },
     {
         pergunta: "Na missão 'Recupere o sasuke' quem sao os ninjas que vao para a missao?",
-        opcao1: ["Sakura, Ino, tenten , konan , kurenai e hinata"],
-        opcao2: ["Naruto , Kiba , Shino , Neji , Choji e Shikamaru"],
-        opcao3: ["Pain , Kisame , Itachi , Kakuzu, Hidan e Sasori"],
-        opcao4: ["Kakashi , Guy , Asuma , hiruzen , iruka e Jiraia"],
+        opcoes:[
+            ["Sakura, Ino, tenten , konan , kurenai e hinata"],
+            ["Naruto , Kiba , Shino , Neji , Choji e Shikamaru"],
+            ["Pain , Kisame , Itachi , Kakuzu, Hidan e Sasori"],
+            ["Kakashi , Guy , Asuma , hiruzen , iruka e Jiraia"],
+        ],
         resposta: "Naruto , Kiba , Shino , Neji , Choji e Shikamaru"
     },
     {
         pergunta: "Quem ensinou ao naruto a tecnica 'rasengan' e qual foi o primeiro a receber esse golpe?",
-        opcao1: ["Kakashi e Sasuke"],
-        opcao2: ["Iruka e Zabuza"],
-        opcao3: ["Jiraia e Kabuto"],
-        opcao4: ["Minato e Pain"],
+        opcoes:[
+            ["Kakashi e Sasuke"],
+            ["Iruka e Zabuza"],
+            ["Jiraia e Kabuto"],
+            ["Minato e Pain"],
+        ],
         resposta: "Jiraia e Kabuto"
     },
     {
         pergunta: "Na luta do exame chunnin, Gaara vs Rock lee, qual seria a melhor trilha sonora?",
-        opcao1: ["Slipknot"],
-        opcao2: ["Iron Maiden"],
-        opcao3: ["Baroes da pisadinha"],
-        opcao4: ["Linkin park"],
+        opcoes:[
+            ["Slipknot"],
+            ["Iron Maiden"],
+            ["Baroes da pisadinha"],
+            ["Linkin park"],
+        ],
         resposta: "Linkin park"
     },
     {
         pergunta: "Qual o nome da raposa de 9 caudas?",
-        opcao1: ["Kurama"],
-        opcao2: ["Son Goku"],
-        opcao3: ["Shukaku"],
-        opcao4: ["Gyuuki"],
+        opcoes:[
+            ["Kurama"],
+            ["Son Goku"],
+            ["Shukaku"],
+            ["Gyuuki"],
+        ],
         resposta: "Kurama"
     }
-*/
 ];  
 
 questoes.forEach(function(questao) {
@@ -181,6 +218,12 @@ const resultadoElemento = document.querySelector('#resultado');
 const pontuacaoElemento = document.querySelector('.encerramento:first-child');
 const mensagemElemento = document.querySelector('.encerramento:last-child')
 const palavraElemento = document.querySelectorAll('.palavra');
+const imagemElemento = document.querySelector('#imagem');
+const criarImagem = document.createElement('img');
+
+
+criarImagem.src = '../img/iruka idle.png';
+imagemElemento.appendChild(criarImagem);
 
 function carregarQuestao(){
     const currentQuestionData = questoes[currentQuestion];
@@ -230,21 +273,31 @@ function selectOption(option){
     if (option === currentQuestionData.resposta) {
         pontuacao++;
         resultadoElemento.style.display = 'inline-block';
+        criarImagem.src = '../img/iruka feliz.png';
+        imagemElemento.appendChild(criarImagem);
         resultadoElemento.textContent = 'Resposta correta!! :D'
         setTimeout(function(){
             resultadoElemento.textContent = '';
+            imagemElemento.style.display = 'block';
         },3000); 
     } else {
+        criarImagem.src = '../img/iruka-irritado.png';
+        imagemElemento.appendChild(criarImagem);
         resultadoElemento.style.display = 'inline-block';
         resultadoElemento.textContent = 'Errou a pergunta :(';
         setTimeout(function(){
             resultadoElemento.textContent = '';
+            imagemElemento.style.display = 'block';
         },3000)
     }
     currentQuestion++;
     if (currentQuestion < questoes.length){
         setTimeout(function(){
-            carregarQuestao(resultadoElemento.style.display = 'none');
+            imagemElemento.style.display = 'block';
+            criarImagem.src = '../img/iruka idle.png';
+            imagemElemento.appendChild(criarImagem);
+            resultadoElemento.style.display = 'none'
+            carregarQuestao();
         },3000);
     } else {
             setTimeout(function(){
@@ -263,6 +316,8 @@ function selectOption(option){
                 texto.style.display = 'block';
                 sim.style.display = 'block';
                 nao.style.display = 'block';
+                criarImagem.src = '../img/iruka_certeza-oficial-ultimate.png'
+                imagemElemento.appendChild(criarImagem);
             },3000);
             alternativas.forEach(function(alternativa){
                 alternativa.addEventListener('click' , function(){
@@ -271,16 +326,19 @@ function selectOption(option){
                             texto.style.display = 'none';
                             sim.style.display = 'none';
                             nao.style.display = 'none';
-                            //encerramentoElemento.style.display = 'block';
+                            criarImagem.src = '../img/iruka adeus.png'
+                            imagemElemento.appendChild(criarImagem);
                             pontuacaoElemento.style.display = 'block';
                             mensagemElemento.style.display = 'block'
-                            pontuacaoElemento.textContent = `Voce acertou ${pontuacao} questoes`;
+                            pontuacaoElemento.textContent = `Voce acertou ${pontuacao} de 15 questoes`;
                             mensagemElemento.textContent = 'GAME OVER. THANKS FOR PLAYNG ^^';
                             setTimeout(function(){
                                 window.location.href = "index.html"
                             },7000);
                             break;
                         case nao:
+                            criarImagem.src = '../img/iruka adeus.png'
+                            imagemElemento.appendChild(criarImagem);
                             texto.style.display = 'none';
                             sim.style.display = 'none';
                             nao.style.display = 'none';
